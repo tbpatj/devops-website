6edb5efb77c5fcfa77634adc9b5c5b0d123b3782
tbpatj <timothy@Timothys-MBP.localdomain>
2021-11-18 12:09:59 -0700

    added functionality for the delete request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const path = require('path');
 const cors = require('cors');
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '03e554d4b00744acab4c379ab4ed8ead',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,30 +20,48 @@ const students = ["Jeddy"];
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-    rollbar.log(`Sent user ${req.ip} the html file`);
+    rollbar.info(`Sent user ${req.ip} the html file`);
 });
 
 app.get("/api/students", (req, res) => {
+    rollbar.info(`${req.ip} got the list of students on page load`);
     res.status(200).send(students);
 });
 
 app.post("/api/students", (req,res) => {
-    students.push(req.body.name);
+    let {name} = req.body;
+    name = name.trim();
+    const index = students.findIndex( studentName => studentName === name);
+    if(index === -1 && name !== ""){
+        students.push(req.body.name);
+        rollbar.log("Student added successfully", {author: "Tuuuj", type: "manual entry"})
+    } else if(name === ""){
+        rollbar.error("no name given");
+        res.status(400).send("Must provide a name");
+    } else {
+        rollbar.error("student already exists");
+        res.status(400).send("that student already exists");
+    }
     res.status(200).send(students);
 })
-
-
-
-// include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '03e554d4b00744acab4c379ab4ed8ead',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+app.delete("/api/students/:id", (req,res) => {
+    let {id} = req.params;
+    if(id < students.length && id >= 0){
+        students.splice(id,1);
+        rollbar.log(`Student ${id} deleted`);
+        res.status(200).send(students);
+    } else {
+        rollbar.error("Student id does not exist");
+        res.status(400).send("No id of input was found in data");
+    }
+    
 })
 
+app.use(rollbar.errorHandler());
+
+
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+//rollbar.log('Hello world!')
 
 
 app.listen(port, () => {
