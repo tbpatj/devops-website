2d4234a1efd9acb23a6d2e0c67f373ac53ebd015
tbpatj <timothy@Timothys-MBP.localdomain>
2021-11-18 11:24:49 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    rollbar.log(`Sent user ${req.ip} the html file`);
+})
+
+const port = process.env.port || 4545;
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '03e554d4b00744acab4c379ab4ed8ead',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+app.listen(port, () => {
+    console.log(`Cover your butts, the port has been opened: ${port}`);
+})
